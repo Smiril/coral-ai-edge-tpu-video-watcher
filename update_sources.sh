6268b8eb641f32b6ec3c4f7959b102235a720b26
Smiril <Smiril@users.noreply.github.com>
2023-04-23 14:04:56 +0200

    Update update_sources.sh

--- update_sources.sh
+++ update_sources.sh
@@ -15,12 +15,12 @@
 # limitations under the License.
 . /etc/os-release
 
-[[ "${NAME}" == "Ubuntu" ]] || exit 0
+[[ "${NAME}" == "Debian" ]] || exit 0
 
 sed -i "s/deb\ /deb \[arch=amd64\]\ /g" /etc/apt/sources.list
 
 cat <<EOT >> /etc/apt/sources.list
-deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME} main universe
-deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-updates main universe
-deb [arch=arm64,armhf] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_CODENAME}-security main universe
+deb [arch=arm64,armhf] http://ftp.at.debian.org/debian bullseye main universe
+deb [arch=arm64,armhf] http://ftp.at.debian.org/debian bullseye-updates main
+deb [arch=arm64,armhf] http://ftp.at.debian.org/debian bullseye-security main
 EOT
